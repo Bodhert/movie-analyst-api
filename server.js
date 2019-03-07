9b7149fa346b0c9fddd00d45b027e9751f256ffd
bodhert <alejandro.cordoba.bodher@gmail.com>
2019-03-07 15:54:36 -0500

    for C/I in jenkins purposes

--- server.js
+++ server.js
@@ -8,21 +8,25 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 3000
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST || 'mysql-test.cxrpknmq0hfi.us-west-2.rds.amazonaws.com',
-  user: process.env.DB_USER || 'applicationuser',
-  password: process.env.DB_PASS || 'applicationuser',
-  database: process.env.DB_NAME || 'movie_db'
-});
+if (process.env.NODE_ENV != 'test') {
+  const connection = mysql.createConnection({
+    host: process.env.DB_HOST || 'mysql-test.cxrpknmq0hfi.us-west-2.rds.amazonaws.com',
+    user: process.env.DB_USER || 'applicationuser',
+    password: process.env.DB_PASS || 'applicationuser',
+    database: process.env.DB_NAME || 'movie_db'
+  });
 
-connection.connect(function (error) {
-  if (error) {
-    console.log('eye with that my hand, error connecting to the DB')
-    console.log(error)
-  } else {
-    console.log('the good, db is conected')
-  }
-});
+  connection.connect(function (error) {
+    if (error) {
+      console.log('eye with that my hand, error connecting to the DB')
+      console.log(error)
+    } else {
+      console.log('the good, db is conected')
+    }
+  });
+}
+
+
 
 // Testing endpoint
 app.get('/', function (req, res) {
@@ -61,7 +65,7 @@ function getAllfromTable(table, callback) {
 
 app.get('/ip', function (req, res, next) {
 
-http.get('http://169.254.169.254/latest/meta-data/local-hostname', (resp) => {
+  http.get('http://169.254.169.254/latest/meta-data/local-hostname', (resp) => {
     let data = '';
 
     resp.on('data', (chunk) => {
--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
-process.env.NODE_ENV = 'test';
+const envPath = __dirname + "../.env";
+require('dotenv').config({path:envPath})
 
-//Require the dev-dependencies
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../server');
